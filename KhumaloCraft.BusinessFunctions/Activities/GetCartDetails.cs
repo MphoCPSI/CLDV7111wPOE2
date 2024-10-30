b8fc8cd3528437aeea6621e80fa0cd1ed8833f3d
tnaylor <thomasnaylor@outlook.com>
2024-10-30 19:59:30 +0200

    #63 - get cart activity return cart by cart id

--- KhumaloCraft.BusinessFunctions/Activities/GetCartDetails.cs
+++ KhumaloCraft.BusinessFunctions/Activities/GetCartDetails.cs
@@ -1,13 +1,28 @@
+using KhumaloCraft.Business.Services;
+using KhumaloCraft.Shared.DTOs;
 using Microsoft.Azure.Functions.Worker;
 
 namespace KhumaloCraft.BusinessFunctions.Activities;
 
-public static class GetCartDetailsActivity
+public class GetCartDetailsActivity
 {
-  [Function("GetCartDetails")]
-  public static Task<string> Run([ActivityTrigger] string cartId)
+  private readonly CartService _cartService;
+
+  public GetCartDetailsActivity(CartService cartService)
   {
-    Console.WriteLine("CartId: {0}", cartId);
-    return Task.FromResult($"fetching the cart: {cartId}");
+    _cartService = cartService;
+  }
+
+  [Function("GetCartDetails")]
+  public async Task<CartDTO> Run([ActivityTrigger] string cartId)
+  {
+    var cart = _cartService.GetCartById(cartId);
+
+    if (cart == null)
+    {
+      return null;
+    }
+
+    return await Task.FromResult(cart);
   }
 }
